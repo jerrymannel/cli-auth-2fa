bfbddcb6d893864f39dac631701fa3e201f69088
Jerry M <developer.jerrymannel@gmail.com>
2022-12-30 23:49:11 +0530

    working

---
+++ app.js
@@ -0,0 +1,59 @@
+const twofactor = require("node-2fa");
+const path = require("path");
+const colors = require('@colors/colors');
+
+function stringComparison(a, b) {
+	const nameA = a.issuer.toUpperCase() || "";
+	const nameB = b.issuer.toUpperCase() || "";
+	if (nameA < nameB) return -1;
+	if (nameA > nameB) return 1;
+	return 0;
+}
+
+const data = require("./db.json").sort(stringComparison)
+
+let issuerLength = 0
+let nameLength = 0
+
+data.forEach(d => {
+	if (d.name.length > nameLength) nameLength = d.name.length
+	if (d.issuer.length > issuerLength) issuerLength = d.issuer.length
+})
+
+const totalLength = issuerLength + nameLength + 6 + 2 + 11
+
+function padding(s, max) {
+	while (s.length < max) {
+		s += " "
+	}
+	return s
+}
+
+function padNum(num, size) {
+  num = num.toString();
+  while (num.length < size) num = "0" + num;
+  return num;
+}
+
+function draw(s,m,e){
+	let i = 0;
+	let mm = "";
+	while(i < totalLength){
+		mm += m;
+		i++;
+	}
+	console.log(`${s}${mm}${e}`);
+}
+
+function regenerate() {
+	data.forEach((d, index) => {
+		let link = new URL(d.url)
+		let newToken = twofactor.generateToken(link.searchParams.get("secret"));
+		console.log(`│ ${colors.green(padNum(index + 1, 2))} │ ${padding(d.issuer, issuerLength)} │ ${padding(d.name, nameLength)} │ ${colors.yellow(newToken.token)} │`)
+	});
+}
+draw("┌","─","┐")
+regenerate()
+draw("└","─","┘")
+
+// setInterval(regenerate, 1000)
